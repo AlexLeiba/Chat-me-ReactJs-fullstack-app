f559527a5f7a74dfb9bb2ecaa87ece3d63c0b8c1
Alex Leiba <alexandru@enovatika.com>
2025-01-01 16:54:24 +0200

    dsds

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 // import { cookies } from 'next/headers'; // For server-side cookie handling
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:5001/api'
-      : '/api',
+  baseURL: '/api',
   withCredentials: true, // Ensures cookies are included in requests
 });
 
