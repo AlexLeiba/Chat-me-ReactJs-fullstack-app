99e2943849dda10fd140b13e0d49c8ef61888489
Alex Leiba <alexandru@enovatika.com>
2025-01-02 13:35:40 +0200

    dd

--- frontend/src/components/skeletons/SidebarSkeleton.jsx
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -4,19 +4,19 @@ const SidebarSkeleton = () => {
 
   return (
     <aside
-      className='h-full w-20 lg:w-72  border-base-300 
+      className='h-full w-20 lg:w-72 border-r border-base-300 
     flex flex-col transition-all duration-200'
     >
-      {/* CONTACTS */}
-      <div className='overflow-y-auto w-full py-2'>
-        {skeletonContacts.map((_, id) => (
-          <div key={id} className='w-full p-2 flex items-center gap-3'>
-            {/* AVATAR */}
+      {/* Skeleton Contacts */}
+      <div className='overflow-y-auto w-full py-3'>
+        {skeletonContacts.map((_, idx) => (
+          <div key={idx} className='w-full p-3 flex items-center gap-3'>
+            {/* Avatar skeleton */}
             <div className='relative mx-auto lg:mx-0'>
-              <div className='skeleton size-10 rounded-full' />
+              <div className='skeleton size-12 rounded-full' />
             </div>
 
-            {/* USER INFO */}
+            {/* User info skeleton - only visible on larger screens */}
             <div className='hidden lg:block text-left min-w-0 flex-1'>
               <div className='skeleton h-4 w-32 mb-2' />
               <div className='skeleton h-3 w-16' />
