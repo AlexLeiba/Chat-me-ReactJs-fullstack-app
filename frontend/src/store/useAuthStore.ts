2fd18a10a5755532ae50342f8efb57cd5ee4a7e8
Alex Leiba <alexandru@enovatika.com>
2025-01-05 16:18:59 +0200

    store

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -58,7 +58,7 @@ type Store = {
   onlineUsers: string[];
 };
 
-const BACKEND_BASE_URL = 'http://localhost:5001'; //'/';
+const BACKEND_BASE_URL = '/';
 // import.meta.env.MODE === 'development' ? 'http://localhost:5001' : '/';
 
 export type FormType = Zod.infer<typeof SignupSchema>;
